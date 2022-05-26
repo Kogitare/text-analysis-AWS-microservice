4846536d547179263e1e67e4933e531c1c862ad3
Karol Augustyniak <karol.aug1@gmail.com>
2022-05-26 14:23:46 +0200

    Organize functions
    Move all fibonacci related operations to fibonacci_tool.py, change function in text_analysis, update main, comment.

--- fibonacci_tool.py
+++ fibonacci_tool.py
@@ -15,6 +15,14 @@ def find_fibo_id(number: int) -> int:
     pos = int(ln(number * sqrt(5) + 1.0/2, phi))
     return pos if pos > 0 else 1
 
-def get_fibo(pos: int) -> int:
+def get_fibo_num(pos: int) -> int:
     """Get fibonacci number in specific position."""
     return fibonacci(pos)
+
+def get_prev_next_fibo(integer: int) -> list:
+    """Create list with previous and next fibonacci number (replaced by None if not fibonacci number)."""
+    if is_in_fibo(integer):
+        id_in_fib = find_fibo_id(integer) - 1
+        return [get_fibo_num(id_in_fib), integer, get_fibo_num(id_in_fib+2)]
+    else:
+        return [None, integer, None]
--- text_analysis.py
+++ text_analysis.py
@@ -1,31 +1,20 @@
 # Text analysis functions
 
-import re
-import fibonacci_tool
+import re   # regex library
+from fibonacci_tool import get_prev_next_fibo
 
-def get_integers(text: str):
+def get_integers(text: str) -> list:
+    """Finds all integers in a string."""
     matches = list(set([int(num) for num in re.findall(r'[0-9]+', text)]))
     matches.sort()
     return matches
 
-def get_fibonacci(int_list: list):
-    for integer, i in zip(int_list, range(len(int_list))):
-        if fibonacci_tool.is_in_fibo(integer):
-            fib_sequence = []
-            id_in_fib = fibonacci_tool.find_fibo_id(integer) - 1
-            fib_sequence.append(fibonacci_tool.get_fibo(id_in_fib))
-            fib_sequence.append(integer)
-            fib_sequence.append(fibonacci_tool.get_fibo(id_in_fib+2))
-            int_list[i] = fib_sequence
-        else:
-            int_list[i] = [None, integer, None]
-    return int_list
-            
-
 def get_words(text: str):
+    """Finds all words (and/or character sets) based on latin alphabet."""
     matches = [num.lower() for num in re.findall(r'[a-zA-Z]+', text)]
     unique_matches = list(set(matches))
-    words = {}
-    for match in unique_matches:
-        words[match] = matches.count(match)
-    return words
+    return {match: matches.count(match) for match in unique_matches}
+
+def create_fibo_table(integer_list: list) -> list[list]:
+    """Creates a table where rows consist of integers and (if they belong) fibonacci numbers."""
+    return [get_prev_next_fibo(integer) for integer in integer_list]
