a12b691a8781111f468d569d1459eff6c9dd49aa
Karol Augustyniak <karol.aug1@gmail.com>
2022-05-26 13:22:58 +0200

    Implement sympy library
    Use sympy library to increase precision.

--- fibonacci_tool.py
+++ fibonacci_tool.py
@@ -1,18 +1,19 @@
 # Functions used in fibonacci related operations
 
-import math
+from sympy import fibonacci, sqrt, N, ln
 
-phi = (1 + math.sqrt(5))/2
+phi = N((1 + sqrt(5))/2, 20)  # phi number for mathematical operations
 
-def is_in_fibo(number: int):
-    check_equation = lambda sign: math.sqrt(5* number**2 +(4*sign)) - int(math.sqrt(5* number**2 +(4*sign)))
-    if check_equation(1) == 0 or check_equation(-1) == 0: return True
+def is_in_fibo(number: int) -> bool:
+    """Check if number is part of the fibonacci sequence."""
+    test_equation = lambda sign: sqrt(5 * number**2 +(4 * sign)) - int(sqrt(5 * number**2 +(4 * sign)))
+    if test_equation(1) == 0 or test_equation(-1) == 0: return True
     else: return False
 
-def find_fibo_id(number: int):
-    return int(math.log(number*math.sqrt(5) + 1.0/2, phi))
+def find_fibo_id(number: int) -> int:
+    """Find position of a number in fibonacci sequence."""
+    return int(ln(number * sqrt(5) + 1.0/2, phi))
 
-def get_fibo(number: int):
-    result = int((phi**number - (-phi)**(-number)) / math.sqrt(5))
-    if result < 0: return 0
-    else: return result
+def get_fibo(pos: int) -> int:
+    """Get fibonacci number in specific position."""
+    return fibonacci(pos)
