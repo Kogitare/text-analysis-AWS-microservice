2614a99a70b56d6d9a5c41de6bbe73b0f2fc006e
Karol Augustyniak <karol.aug1@gmail.com>
2022-05-26 21:15:58 +0200

    Add build script
    Script creates .zip file that can be exported to AWS Lambda

---
+++ build.sh
@@ -0,0 +1,20 @@
+rm -r build
+mkdir build
+cd src
+echo "### Created build folder."
+pipenv requirements > ../build/requirements.txt
+cd ../build
+python3 -m venv ./
+source bin/activate
+python3 -m pip install -r requirements.txt
+deactivate
+echo "### Installed dependencies in virtual environment."
+cd lib/python3.10/site-packages
+zip -r ../../../../deployment_package.zip .
+echo "### Created deployment_package.zip"
+cd ../../../../src
+zip -g ../deployment_package.zip ./*.py
+echo "### Added main scripts to deployment_package.zip"
+cd ../
+rm -r build
+echo "### Removed build folder."
